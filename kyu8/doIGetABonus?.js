62569ecad866b10c4ca2258632e5da128c951f97
JWNg <JWNg@users.noreply.github.com>
2016-07-23 06:19:24 -0400

    Update doIGetABonus?.js

--- kyu8/doIGetABonus?.js
+++ kyu8/doIGetABonus?.js
@@ -7,3 +7,8 @@
 //If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
 //
 //Return the total figure the individual will receive as a string prefixed with '£' (JS and Java) or '$' (C#, C++, Ruby and Python).
+
+function bonusTime(salary, bonus) {
+  if (bonus === true) { return  '£'+ salary*10};
+  return '£' + salary;
+}
