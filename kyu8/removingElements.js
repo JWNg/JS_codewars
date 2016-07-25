5f4e0504b23708883367be1eb7d05ed92e8b256d
JWNg <JWNg@users.noreply.github.com>
2016-07-24 20:46:15 -0400

    Update removingElements.js

--- kyu8/removingElements.js
+++ kyu8/removingElements.js
@@ -5,3 +5,10 @@
 //myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
 //None of the arrays will be empty, so you don't have to worry about that!
 //
+function removeEveryOther(arr){
+  var newArr = [];
+  for (var i = 0 ; i < arr.length ; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
