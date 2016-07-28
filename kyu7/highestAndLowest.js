6be38e90c88e7976b901bdc1878d51c4f6b154a4
JWNg <JWNg@users.noreply.github.com>
2016-07-28 10:21:15 -0400

    Update highestAndLowest.js

--- kyu7/highestAndLowest.js
+++ kyu7/highestAndLowest.js
@@ -12,3 +12,13 @@
 //All numbers are valid Int32, no need to validate them.
 //There will always be at least one number in the input string.
 //Output string must be two numbers separated by a single space, and highest number is first.//
+
+
+function highAndLow(numbers){
+  var arr = numbers.split(" ");
+  var sortedArr = arr.sort(function(a,b) {
+    return a - b ;
+  });
+  var endPosition = sortedArr.length - 1;
+  return sortedArr[endPosition] + " " + sortedArr[0];
+}
