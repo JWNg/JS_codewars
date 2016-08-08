328db3949d5a0e61cd29565f2088e8477f934624
JWNg <JWNg@users.noreply.github.com>
2016-08-08 05:29:13 -0400

    Create sumOfTwoLowestIntegers.js

---
+++ kyu7/sumOfTwoLowestIntegers.js
@@ -0,0 +1,7 @@
+//Create a function that calculates the sum of the two lowest numbers given an array of 5 positive integers. No floats or empty arrays will be passed.
+//
+//For example, when an array is passed like [19,5,42,2,77], the output should be 7.
+//
+//[10,343445353,3453445,3453545353453] should return 3453455.
+//FUNDAMENTALSARRAYS
+
