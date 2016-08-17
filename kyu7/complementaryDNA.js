3d75eb81f44989d4200df35a0c4bc0e867dd0e24
JWNg <JWNg@users.noreply.github.com>
2016-08-17 11:25:10 -0400

    Update complementaryDNA.js

--- kyu7/complementaryDNA.js
+++ kyu7/complementaryDNA.js
@@ -40,3 +40,10 @@ function DNAStrand(dna){
 //  C: 'G',
 //  G: 'C',
 //}
+
+
+//var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+//
+//function DNAStrand(dna){
+//  return dna.split('').map(function(v){ return pairs[v] }).join('');
+//}//
