c30cae4a4a9999e15da6e28ab62fae1fae2d5ab9
JWNg <JWNg@users.noreply.github.com>
2016-08-25 05:24:09 -0400

    Update evenOrOdd.js

--- kyu8/evenOrOdd.js
+++ kyu8/evenOrOdd.js
@@ -1,2 +1,7 @@
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 //FUNDAMENTALS
+
+function even_or_odd(number) {
+  if (number%2 === 0) { return "Even" };
+  return "Odd"
+}
