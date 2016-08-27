d2b32e5a9dace460cbae89e614c89346e77ba2b1
JWNg <JWNg@users.noreply.github.com>
2016-08-27 05:42:27 -0400

    Create enumerableMagic-1TrueForAll.js

---
+++ kyu8/enumerableMagic-1TrueForAll.js
@@ -0,0 +1,4 @@
+//Create a method all? (JS: all) which takes an array, and a block (JS: function), and returns true if the block (/function) returns true for every element in the array. Otherwise, it should return false. If the array is empty, it should return true, since technically nothing failed the block (/function) test.
+
+//Here's a (Ruby) resource if you get stuck: http://www.rubycuts.com/enum-all
+//FUNDAMENTALSARRAYS//
