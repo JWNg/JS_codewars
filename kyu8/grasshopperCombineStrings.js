e2eec62f8cfee6fee97eda40fc3f303ca70609b4
JWNg <JWNg@users.noreply.github.com>
2016-08-31 05:24:58 -0400

    Create grasshopperCombineStrings.js

---
+++ kyu8/grasshopperCombineStrings.js
@@ -0,0 +1,16 @@
+//Combine strings function
+//
+//Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name). The function should return the full name.
+//
+//Example:
+//
+//combineNames('James', 'Stevens')
+//returns:
+//
+//'James Stevens'
+//FUNDAMENTALS
+
+
+function combineNames(first, last) {
+  return first +" "+ last;
+}
