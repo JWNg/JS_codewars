4ba70ea4b35edc855cc64cc4dbf91067ce1f1a45
JWNg <JWNg@users.noreply.github.com>
2016-09-04 19:44:16 -0400

    Update regexpBasics_IsItALetter.js

--- kyu7/regexpBasics_IsItALetter.js
+++ kyu7/regexpBasics_IsItALetter.js
@@ -2,3 +2,7 @@
 //
 //Implement String#letter? (Ruby), StringUtils.isLetter(String) (Java), String.prototype.isLetter() (JavaScript) or letter? (Clojure), which should return true if given object is a single ASCII letter (lower or upper case), false otherwise.
 //FUNDAMENTALSREGULAR EXPRESSIONSDECLARATIVE PROGRAMMINGADVANCED LANGUAGE FEATURESSTRINGS
+String.prototype.isLetter = function() {
+  let str = this.toString();
+  return /^[a-z]$/i.test(str);
+}
