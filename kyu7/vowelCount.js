70f6c97b28f8a78ad0c50587d101af9f942d02dc
JWNg <JWNg@users.noreply.github.com>
2016-09-17 06:11:38 -0400

    Update vowelCount.js

--- kyu7/vowelCount.js
+++ kyu7/vowelCount.js
@@ -2,3 +2,13 @@
 //
 //We will consider a, e, i, o, and u as vowels for this Kata.
 //FUNDAMENTALSSTRINGSUTILITIES//
+
+function getCount(str) {
+  var vowelMatch = function(l){ return l.match(/[aeiou]/i)},  
+      found = str.split('').filter(vowelMatch)
+  return found.join('').length;
+}
+
+function getCount(str) {
+  return (str.match(/[aeiou]/ig)||[]).length;
+}
