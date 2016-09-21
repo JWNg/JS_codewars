ea36c371bba333f809e0ed6e9a6e1240b6151642
JWNg <JWNg@users.noreply.github.com>
2016-09-21 10:25:02 -0400

    Create squareEveryDigit.js

---
+++ kyu7/squareEveryDigit.js
@@ -0,0 +1,12 @@
+//Description:
+//
+//Welcome. In this kata, you are asked to square every digit of a number.
+//
+//For example, if we run 9119 through the function, 811181 will come out.
+//
+//Note: The function accepts an integer and returns an integer
+//FUNDAMENTALSMATHEMATICSALGORITHMSNUMBERS
+
+function squareDigits(num){
+  return parseInt(num.toString().split('').map( fig => {return fig * fig }).join(''))
+}
