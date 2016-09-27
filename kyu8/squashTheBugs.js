f44f6bfaed4689252980aafb1b24c62fd9e122d6
JWNg <JWNg@users.noreply.github.com>
2016-09-27 12:50:52 -0400

    Create squashTheBugs.js

---
+++ kyu8/squashTheBugs.js
@@ -0,0 +1,8 @@
+//Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+//
+//There will only be one 'longest' word.
+//BUGSFUNDAMENTALS
+//
+function findLongest(str) {
+  return str.split(' ').sort((first,second) => { return (second.length) - (first.length) } )[0].length
+}
