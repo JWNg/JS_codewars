d3527f267a6bce22f4bb992cc043c48f8884ef08
JWNg <JWNg@users.noreply.github.com>
2016-09-28 06:27:40 -0400

    Update shortestWord.js

--- kyu7/shortestWord.js
+++ kyu7/shortestWord.js
@@ -3,5 +3,9 @@
 //String will never be empty and you do not need to account for different data types.
 //FUNDAMENTALS
 function findShort(s){
+  return Math.min.apply(null, s.split(' ').map(w => w.length));
+}
+
+function findShort(s){
   return s.split(' ').sort((first,second)=>{return (first.length)-(second.length)})[0].length
 }
