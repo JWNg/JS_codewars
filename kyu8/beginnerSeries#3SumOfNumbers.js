518816b7282bed593967e791c85cb02efcf338ac
JWNg <JWNg@users.noreply.github.com>
2016-10-06 07:21:16 -0400

    Update beginnerSeries#3SumOfNumbers.js

--- kyu8/beginnerSeries#3SumOfNumbers.js
+++ kyu8/beginnerSeries#3SumOfNumbers.js
@@ -20,3 +20,9 @@ function GetSum( a,b ) {
   }  
   return sum.reduce((pre,cur) =>{return pre+cur});
 }
+
+//const GetSum = (a, b) => {
+//  let min = Math.min(a, b),
+//      max = Math.max(a, b);
+//  return (max - min + 1) * (min + max) / 2;
+//}
