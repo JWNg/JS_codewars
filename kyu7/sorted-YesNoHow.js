7b940cdf741973bc1e07dd292420bd492e103e91
JWNg <JWNg@users.noreply.github.com>
2016-10-22 07:55:23 -0400

    Update sorted-YesNoHow.js

--- kyu7/sorted-YesNoHow.js
+++ kyu7/sorted-YesNoHow.js
@@ -5,3 +5,23 @@ Implement the method isSortedAndHow, which accepts an array of integers, and ret
 'no'
 You can assume the array will always be valid, and there will always be one correct answer.
 FUNDAMENTALSARRAYSSORTINGALGORITHMS
+
+
+function isSortedAndHow(array) {
+  var asc = array.slice().sort((a,b)=>{return a-b})
+  var desc= array.slice().sort((a,b)=>{return b-a});
+  if( array.every(function(element, index) {
+      return element === asc[index]; 
+    })
+  ) { return 'yes, ascending' };
+    if( array.every(function(element, index) {
+      return element === desc[index]; 
+    })
+  ) { return 'yes, descending' };
+  return 'no';
+}
+
+function isSortedAndHow(arr) {
+  return arr.every((x,i)=>i==0||arr[i]>=arr[i-1])?'yes, ascending':
+         arr.every((x,i)=>i==0||arr[i]<=arr[i-1])?'yes, descending':'no'
+}
