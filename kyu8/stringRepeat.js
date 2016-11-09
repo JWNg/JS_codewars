93bb0f2e82868080e5268e169d3266cd8687edd6
JWNg <JWNg@users.noreply.github.com>
2016-11-09 06:06:35 -0500

    Create stringRepeat.js

---
+++ kyu8/stringRepeat.js
@@ -0,0 +1,9 @@
+Write a function called repeat_str which repeats the given string src exactly count times.
+
+repeat_str(3, "foo"); // "foofoofoo"
+repeat_str(1, "bar spam"); // "bar spam"
+FUNDAMENTALS
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
