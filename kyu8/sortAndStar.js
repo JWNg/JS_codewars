32151a3f2505a7784d80153acd47f411ef55b398
JWNg <JWNg@users.noreply.github.com>
2016-12-08 05:46:37 -0500

    Create sortAndStar.js

---
+++ kyu8/sortAndStar.js
@@ -0,0 +1,10 @@
+Description:
+
+You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
+
+The returned value must be a string, and have '***' between each of its letters.
+FUNDAMENTALSSTRINGSARRAYS
+
+function twoSort(s) {
+  return s.sort((word1, word2) => {return word1.localeCompare(word2) })[0].split('').join('***');
+}
