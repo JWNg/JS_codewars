a77c876ede7175c44b8f696c921fc82cdb87679b
JWNg <JWNg@users.noreply.github.com>
2017-01-12 07:51:44 -0500

    Create helpSuzukiCompleteHisChores.js

---
+++ kyu7/helpSuzukiCompleteHisChores.js
@@ -0,0 +1,28 @@
+Description:
+
+Suzuki has a long list of chores required to keep the monastery in good order. Each chore can be completed independent of the others and assigned to any student. He needs to assign two chores to each student in a way which minimizes the total duration of the day's work. There will always be an even number of chores and enough students to complete them.
+
+You will be given an array containing the estimated duration of each chore such as:
+
+10 ≤ chores length ≤ 30
+
+chores = [1, 5, 2, 8, 4, 9, 6, 4, 2, 2, 2, 9]
+Return an array with each students work duration in ascending order such that the last element represents the total duration of the day's work. Each integer in the return array is the sum of each students two assigned chores.
+
+chores = [1, 5, 2, 8, 4, 9, 6, 4, 2, 2, 2, 9]
+
+Chore 1 = 2hrs 
+Chore 2 = 9hrs
+
+2 + 9 = 11 hours duration
+
+return [7, 8, 8, 10, 10, 11]
+Please also try the other Kata in this series..
+
+Help Suzuki count his vegetables...
+Help Suzuki pack his coal basket!
+Help Suzuki purchase his Tofu!
+Help Suzuki rake his garden!
+Suzuki needs help lining up his students!
+How many stairs will Suzuki climb in 20 years?
+ALGORITHMS
