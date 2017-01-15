aac2e0ac88889807974ea936f08546f7eb58f570
JWNg <JWNg@users.noreply.github.com>
2017-01-15 07:21:45 -0500

    Create makeASquareBox.js

---
+++ kyu7/makeASquareBox.js
@@ -0,0 +1,24 @@
+Description:
+
+Easy; Make a box
+
+Given a number as a parameter, return an array containing strings which form a box.
+Like this:
+
+n = 5
+
+[
+  '-----',
+  '-   -',
+  '-   -',
+  '-   -',
+  '-----'
+]
+n = 3
+
+[
+  '---',
+  '- -',
+  '---'
+]
+ALGORITHMSSTRINGSARRAYS
