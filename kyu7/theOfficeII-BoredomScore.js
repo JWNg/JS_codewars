342f602bcffdfbe44043be7a2f1fce8b861a4f0d
JWNg <JWNg@users.noreply.github.com>
2017-02-01 05:52:31 -0500

    Update theOfficeII-BoredomScore.js

--- kyu7/theOfficeII-BoredomScore.js
+++ kyu7/theOfficeII-BoredomScore.js
@@ -27,3 +27,21 @@ The Office I - Outed
 The Office III - Broken Photocopier
 The Office IV - Find a Meeting Room
 The Office V - Find a Chair
+
+function boredom(staff) {
+  var teams = { 'accounts'      : 1,
+                'finance'       : 2 ,
+                'canteen'       : 10, 
+                'regulation'    : 3, 
+                'trading'      : 6, 
+                'change'        : 6,
+                'IS'            : 8,
+                'retail'        : 5,
+                'cleaning'      : 4,
+                'pissing about' : 25},
+      score = Object.keys(staff)
+                    .map(key=>teams[staff[key]])
+                    .reduce((a,b) => a+b, 0);
+                    
+return score <= 80 ? 'kill me now' : (score > 100 ? 'party time!!' : 'i can handle this');            
+}
