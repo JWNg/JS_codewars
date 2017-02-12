bc3057aa225839e845ce891f8410b56dfe0210f9
JWNg <JWNg@users.noreply.github.com>
2017-02-12 08:59:23 -0500

    Create exclamationMarksSeries#4:RemoveAllExclamationMarksFromSentenceButEnsureAExclamationMarktTheEndOfString.js

---
+++ kyu8/exclamationMarksSeries#4:RemoveAllExclamationMarksFromSentenceButEnsureAExclamationMarktTheEndOfString.js
@@ -0,0 +1,19 @@
+Description:
+
+Description:
+
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+Examples
+
+remove("Hi!") === "Hi!"
+remove("Hi!!!") === "Hi!"
+remove("!Hi") === "Hi!"
+remove("!Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!"
+remove("Hi") === "Hi!"
+(For C and clojure please use remove_bang as the function name. Also, in C it is expected that the pointer returned by the function was allocated by malloc.
+
+Note
+
+Please don't post issue about difficulty or duplicate.
+FUNDAMENTALS
