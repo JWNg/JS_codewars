2c0a49ba966e0ac92dd33f4a6e807e9922a91b35
JWNg <JWNg@users.noreply.github.com>
2017-02-15 06:11:57 -0500

    Update addingValuesOfArraysInAShiftedWay.js

--- kyu7/addingValuesOfArraysInAShiftedWay.js
+++ kyu7/addingValuesOfArraysInAShiftedWay.js
@@ -42,3 +42,15 @@ Have fun coding it and please don't forget to vote and rank this kata! :-)
 
 I have created other katas. Have a look if you like coding and challenges.
 ALGORITHMSBASIC LANGUAGE FEATURESFUNDAMENTALSMATHEMATICSNUMBERSARRAYS
+
+function addingShifted (arrays, shift) {
+  let result = [];
+  let start = 0;
+  for (let array of arrays) {
+    for (let i = 0; i < array.length; i += 1) {
+      result[start + i] = (result[start + i] || 0) + array[i];
+    }
+    start += shift;
+  }
+  return result;
+}
