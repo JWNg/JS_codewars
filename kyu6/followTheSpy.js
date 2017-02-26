84caf17d33feb83f1fd7eb85074852fb34ccb5c6
JWNg <JWNg@users.noreply.github.com>
2017-02-26 07:16:05 -0500

    Update followTheSpy.js

--- kyu6/followTheSpy.js
+++ kyu6/followTheSpy.js
@@ -10,3 +10,20 @@ result: "USA, BRA, UAE, JPN, PHL"
 
 note: It is safe to assume that there will be no repeating place with different route and there are no empty routes and could have at least one (1) route (from one waypoint to another).
 ALGORITHMS
+
+
+// it should return the places from the given routes
+function findRoutes(routes) {
+    if (routes.length > 1) {
+        var currentstop = currentLocation(routes);
+        let remainingRoutes = routes.filter((route)=>{return !(route[0] == currentstop)});
+        return currentstop + ', ' + findRoutes(remainingRoutes);
+    } else {
+        return routes[0][0] + ', ' + routes[0][1];
+    }  
+}
+
+function currentLocation(routes){
+    let stops = routes.map((route)=>{return route[1]});
+    return routes.filter((route)=>{return !stops.includes(route[0])})[0][0];
+}
