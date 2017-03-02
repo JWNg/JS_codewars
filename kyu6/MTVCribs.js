aa34753722a0015c7ae381ad5b54818cacff5845
JWNg <JWNg@users.noreply.github.com>
2017-03-02 06:45:55 -0500

    Update MTVCribs.js

--- kyu6/MTVCribs.js
+++ kyu6/MTVCribs.js
@@ -31,3 +31,25 @@ If you manage to complete it, you can try a harder version here
 
 Good luck!
 FUNDAMENTALSCONTROL FLOWBASIC LANGUAGE FEATURESLOOPS
+
+function myCrib(n) {
+//height (n x 2) + 1
+//width (n x 2) + 2\
+    return roof(n) + square(n);
+}
+
+//space on each side starts at n, n-1 and etc
+function roof(n){
+    var roof = ''    
+    for (var i = n; i >= 0; i --){
+        roof = roof + (i === 0 ? 
+        ' '.repeat(i) + '/' + '_'.repeat((n-i)*2) + '\\' +  ' '.repeat(i) + "\n":
+        ' '.repeat(i) + '/' + ' '.repeat((n-i)*2) + '\\' + ' '.repeat(i) + "\n")
+    }
+    return roof
+}
+
+//
+function square(n){    
+    return ("|" + ' '.repeat(n*2) + "|" + "\n").repeat(n-1) + ("|" + '_'.repeat(n*2) + "|")
+}
