a3ac1fe6ab401a35fe7ad13f3713b458e794228c
JWNg <JWNg@users.noreply.github.com>
2017-03-04 06:57:07 -0500

    Create CamelCaseMethod.js

---
+++ kyu6/CamelCaseMethod.js
@@ -0,0 +1,12 @@
+Write simple .camelcase method (camel_case function in PHP) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+Don't forget to rate this kata! Thanks :)
+ALGORITHMSFUNDAMENTALSSTRINGS
+
+String.prototype.camelCase=function(){
+    return this.split(' ').map((word)=>{return word.charAt(0).toUpperCase() + word.slice(1)}).join('')
+}
