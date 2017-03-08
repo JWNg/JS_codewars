bd0aa866d0d55ddcbda3ac55b5bdeb41a8dfbf5a
JWNg <JWNg@users.noreply.github.com>
2017-03-08 05:31:01 -0500

    Create konttiLanguage.js

---
+++ kyu6/konttiLanguage.js
@@ -0,0 +1,8 @@
+Description:
+
+Kontti language is a finnish word play game, you add -kontti to the end of a word and then switch their first letters until the first vowel (as in "aeiouy"); if no vowel is present, the word stays the same.
+
+For example the word lamppu becomes komppu-lantti; aeiou becomes koeiou-antti and so on.
+
+Write a string method that turns a sentence into a kontti sentence!
+FUNDAMENTALSREGULAR EXPRESSIONSDECLARATIVE PROGRAMMINGADVANCED LANGUAGE FEATURESSTRINGS
