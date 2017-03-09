6ca025799be5c04a630b97a8b0429c86e590031a
JWNg <JWNg@users.noreply.github.com>
2017-03-09 06:55:49 -0500

    Update oneLineTask:TwoLetters.js

--- kyu5/oneLineTask:TwoLetters.js
+++ kyu5/oneLineTask:TwoLetters.js
@@ -1,3 +1,5 @@
 TASK: Convert two letters to their ASCII code(number format) and your code should in one line and less than 54 characters.
 
 PUZZLESGAMES
+
+toASCIINumber=(a,b)=>+Buffer.from(a+b).join``
