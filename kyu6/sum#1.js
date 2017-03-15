fe4db098b97b21e7e2ae693f86ff3e82544ce579
JWNg <JWNg@users.noreply.github.com>
2017-03-15 06:28:16 -0400

    Create sum#1.js

---
+++ kyu6/sum#1.js
@@ -0,0 +1,13 @@
+PUZZLE #1. SUM
+
+Assignment:
+
+Write a function named sum which performs addition in the way shown below
+
+sum(4)(5)(9)(); // => 18
+sum(5)();       // => 5
+sum();          // => 0
+NOTE: Pay attention that last brackets are left empty to indicate end of operations
+
+Thanks to @JohanWiltink, @donaldsebleung
+PUZZLESARITHMETICMATHEMATICSALGORITHMSNUMBERSGAMES
