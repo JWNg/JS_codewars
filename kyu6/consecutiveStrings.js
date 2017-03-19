8292180943ecf4ae4ce65f9c81dd6e34ef997387
JWNg <JWNg@users.noreply.github.com>
2017-03-19 07:41:00 -0400

    Create consecutiveStrings.js

---
+++ kyu6/consecutiveStrings.js
@@ -0,0 +1,17 @@
+Description:
+
+You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Example:
+
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+FUNDAMENTALS
+
+function longestConsec(strarr, k) {
+  if( strarr.length==0 || k> strarr.length || k <1 ) return "";
+  let lens = strarr.map( (_,i,arr) => arr.slice(i,i+k).join('').length ),
+      i = lens.indexOf( Math.max(...lens) );
+  return strarr.slice(i,i+k).join('')
+}
