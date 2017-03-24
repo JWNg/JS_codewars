1fd22a380709a27bab4246df6ae03e71ab602a8a
JWNg <JWNg@users.noreply.github.com>
2017-03-24 06:12:22 -0400

    Update simpleFun#152:InviteMoreWomen?.js

--- kyu7/simpleFun#152:InviteMoreWomen?.js
+++ kyu7/simpleFun#152:InviteMoreWomen?.js
@@ -4,7 +4,7 @@ Task
 
 King Arthur and his knights are having a New Years party. Last year Lancelot was jealous of Arthur, because Arthur had a date and Lancelot did not, and they started a duel.
 
-To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men at this year's party. He gave you a list of integers of all the party goers.
+To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men at this years party. He gave you a list of integers of all the party goers.
 
 Arthur needs you to return true if he needs to invite more women or false if he is all set.
 Input/Output
