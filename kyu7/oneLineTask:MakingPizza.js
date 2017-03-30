bcf8e8e374b4f789f062e26952914b7981b669d0
JWNg <JWNg@users.noreply.github.com>
2017-03-30 06:05:31 -0400

    Update oneLineTask:MakingPizza.js

--- kyu7/oneLineTask:MakingPizza.js
+++ kyu7/oneLineTask:MakingPizza.js
@@ -33,3 +33,21 @@ var hello = "world"  <--- bad way
         x            <--- 1 characters modified
 var hello= "world"   <--- better way
 BUGSPUZZLESGAMES
+
+function makePizza(pieces) { //Let's make n pieces of Pizza ;-)
+  var result = "";
+  while ( pieces-- ){
+    var needSteps = 5;
+    while ( needSteps-- ) result += make( needSteps );
+  }
+  return result;
+}
+function make(step){
+  switch ( step+1){
+    case 5: return "P";
+    case 4: return "i";
+    case 3: 
+    case 2: return "z";
+    case 1: return "a";
+  }
+}
