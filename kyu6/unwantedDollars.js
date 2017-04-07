63002b7546cac1221e88854986f6bc86809d019d
JWNg <JWNg@users.noreply.github.com>
2017-04-07 07:20:16 -0400

    Update unwantedDollars.js

--- kyu6/unwantedDollars.js
+++ kyu6/unwantedDollars.js
@@ -13,3 +13,11 @@ So, we need a simple function that converts a string representing a number (poss
 
 Remember to consider negative numbers (the - sign may come either before or after the $ sign, if there is one), and any extraneous space characters (leading, trailing, or around the $ sign) that the users might put in. If the given string does not represent a number, (either with or without a $ sign), return 0.0 .
 FUNDAMENTALSBASIC LANGUAGE FEATURES
+
+function money_value(s)
+{
+  const regex = /([$ ]*)/g;
+  let sNew = s.replace(regex, '');
+  let sFloat = parseFloat(sNew);
+  return isNaN(sFloat) ? 0 : sFloat;
+}
