72a7997b6964dbb4a079aad3faa5924ad1ca5f75
JWNg <JWNg@users.noreply.github.com>
2017-04-18 06:19:15 -0400

    Update typeChecking.js

--- kyu6/typeChecking.js
+++ kyu6/typeChecking.js
@@ -29,3 +29,25 @@ function Custom(){}
 type(new Custom()) --> 'Custom'
 Related reading: MDN's page for Object.prototype.toString and Object.prototype.constructor.
 FUNDAMENTALSUTILITIESVALIDATIONALGORITHMSCONTROL FLOWBASIC LANGUAGE FEATURESSTRINGSNUMBERS
+
+function type(val) {
+  if(typeof val === 'undefined') return 'Undefined';
+  if(typeof val === 'boolean') return 'Boolean';
+  if(typeof val === 'function') return 'Function';
+  if(typeof val === 'number'){
+    if(isNaN(val)) return 'Number NaN';
+    if(!isFinite(val)) return 'Number Infinite';
+    if(Math.floor(val)===val) return 'Number Integer';
+    else return 'Number Float';
+  }
+  if(typeof val === 'string'){
+    if(isNaN(Number(val))) return 'String';
+    return 'String Numeric';
+  }
+  if(typeof val === 'object'){
+    if(val===null) return 'Null';
+    var val_str = Object.prototype.toString.call(val).split(' ')[1].split(']')[0];
+    if(val_str==='Object'|| val_str==='Error') return val.constructor.name;
+    return val_str;
+    }
+}
