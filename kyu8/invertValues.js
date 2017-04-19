d7d2400254484688d8d25a961f15627d6bee8b1e
JWNg <JWNg@users.noreply.github.com>
2017-04-19 06:08:19 -0400

    Update invertValues.js

--- kyu8/invertValues.js
+++ kyu8/invertValues.js
@@ -9,3 +9,5 @@ invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 You can assume that all values are integers.
 FUNDAMENTALSINTEGERSNUMBERSLISTSDATA STRUCTURES
+
+const invert = array => array.map((current)=>{return current === 0 ? 0 :current*-1})
