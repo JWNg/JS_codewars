2ba5d0ce9d9c2e1788819788f5f3d2849e24b6d2
JWNg <JWNg@users.noreply.github.com>
2017-04-24 09:51:42 -0400

    Create simpleFun#182:HappyG.js

---
+++ kyu7/simpleFun#182:HappyG.js
@@ -0,0 +1,31 @@
+Task
+
+Let's say that "g" is happy in the given string, if there is another "g" immediately to the right or to the left of it.
+
+Find out if all "g"s in the given string are happy.
+Example
+
+For str = "gg0gg3gg0gg", the output should be true.
+
+For str = "gog", the output should be false.
+Input/Output
+
+[input] string str
+
+A random string of lower case letters, numbers and spaces.
+[output] a boolean value
+
+true if all "g"s are happy, false otherwise.
+PUZZLESGAMES
+
+Poweredby_qualified
+
+function gHappy(str) {
+    let split = str.split('')
+    for (i = 0; i < split.length; i++) {
+        if (split[i] == "g" && !(split[i-1] == "g" || split[i+1] == "g")) {
+            return false
+        }
+    }
+    return true;
+}
