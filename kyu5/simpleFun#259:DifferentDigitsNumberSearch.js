48b009c341b5b2140594ee0af399dca566ada591
JWNg <JWNg@users.noreply.github.com>
2017-05-09 14:47:31 -0400

    Update simpleFun#259:DifferentDigitsNumberSearch.js

--- kyu5/simpleFun#259:DifferentDigitsNumberSearch.js
+++ kyu5/simpleFun#259:DifferentDigitsNumberSearch.js
@@ -20,3 +20,23 @@ For arr = [22, 111, 101, 124, 33, 30], the output should be 124
 
 For arr = [1111, 404], the output should be -1.
 FUNDAMENTALS
+
+function differentDigitsNumberSearch(arr) {
+    for (var i=0; i < arr.length; i++) {
+        if (duplicates(arr[i])) {
+            return arr[i]  
+        }
+    }
+    return -1
+}
+
+function duplicates(n) {
+    var newString = "";
+    for (var i = 0; i < (n+"").length; i++){
+        if (!newString.includes((n+'')[i])) {
+            newString += ((n+'')[i])
+        }
+    }
+
+    return (n+'') === newString ? true : false;
+}
