879d61bce7413b088c4dc00351c55968492661b5
JWNg <JWNg@users.noreply.github.com>
2017-05-22 07:49:41 -0400

    Create simpleFun#289:SumTheDifference.js

---
+++ kyu6/simpleFun#289:SumTheDifference.js
@@ -0,0 +1,54 @@
+Task
+
+You're given an equation that consists of numbers, parenthesis and operations '+' and '-'. But you don't want just to calculate it's result the way ordinary people do. You have your own rules of calculation!
+
+Firstly, you remove all whitespaces from the equation (who needs them anyway)?
+
+Secondly, you calculate the sum of even digits in it. If there's a '-' symbol just before the digit, you consider it as a negative number.
+
+Thirdly, you calculate the sum of odd digits, paying attention to the '-' symbols as well.
+
+Finally, you subtract the sum of the odd digits from the sum of even digits. The obtained result is the result of the equation.
+
+Now you need to write a program that does this kind of calculation for you. Given an equation, calculate it's value with your very own algorithm.
+
+Note: equation can be confusing and invalid ;-)
+
+Input/Output
+
+[input] string equation
+
+An equation (not necessarily correct) consisting of digits, parenthesis and operators '-' and '+'.
+
+0 ≤ equation.length ≤ 100
+
+[output] an integer
+
+Result of the equation.
+
+Example
+
+For equation = "(-100) + 100 - (-200) + 1", the output should be -3.
+
+remove the whitespaces:
+
+"(-100)+100-(-200)+1"
+
+there's only one even digit, which is -2;
+
+odd digits sum up to -1 + 1 + 1 = 1;
+
+their difference is -2 - 1 = -3`.
+
+For equation = "(-25) + (-75) + 50 - 40 - (22 + 2)", the output should be -8.
+
+remove the whitespaces:
+
+"(-25)+(-75)+50-40-(22+2)"
+
+even digits sum up to -2 - 4 + 2 + 2 + 2 = 0;
+
+odd digits sum up to 5 - 7 + 5 + 5 = 8;
+
+their difference is 0 - 8 = -8.
+FUNDAMENTALS
