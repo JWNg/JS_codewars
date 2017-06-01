9eca7d89d4de87ca28d525a9767794eb2be4d267
JWNg <JWNg@users.noreply.github.com>
2017-06-01 07:13:52 -0400

    Update Mr.Safety'sTreasures.js

--- kyu6/Mr.Safety'sTreasures.js
+++ kyu6/Mr.Safety'sTreasures.js
@@ -22,3 +22,27 @@ unlokc("toilet"); // => 864538
 PUZZLESGAMES
 
 
+function unlock(str){
+  str=str.toLowerCase()
+  var output = ''
+  for(var i = 0; i<str.length; i++){
+    if(["a","b","c"].includes(str[i])){
+      output=output.concat(2)
+    } else if(["d","e","f"].includes(str[i])){
+      output=output.concat(3)
+    } else if(["g","h","i"].includes(str[i])){
+      output=output.concat(4)
+    } else if(["j","k","l"].includes(str[i])){
+      output=output.concat(5)
+    } else if(["m","n","o"].includes(str[i])){
+      output=output.concat(6)
+    } else if(["p","q","r","s"].includes(str[i])){
+      output=output.concat(7)
+    } else if(["t","u","v"].includes(str[i])){
+      output=output.concat(8)
+    } else {
+    output=output.concat(9)
+    }
+  }
+   return output;
+}
