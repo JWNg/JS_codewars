3ffc512d5814dc0a021513dcea1b19a75bbb031f
JWNg <JWNg@users.noreply.github.com>
2017-06-08 19:59:40 -0400

    Create ASCIIFun#1:X-Shape.js

---
+++ kyu6/ASCIIFun#1:X-Shape.js
@@ -0,0 +1,24 @@
+This will probably be a little serie :)
+
+X-Shape
+
+You will get an odd Integer n (>= 3) and your task is to draw a X. Each line is seperated with '\n'.
+
+Use the following characters:
+
+■ □
+
+e.g.:
+
+
+                                     ■□□□■
+            ■□■                      □■□■□
+  x(3) =>   □■□              x(5)=>  □□■□□
+            ■□■                      □■□■□
+                                     ■□□□■
+
+Serie: ASCII Fun
+
+ASCII Fun #1: X-Shape
+ASCII Fun #2: Funny Dots
+ALGORITHMS
