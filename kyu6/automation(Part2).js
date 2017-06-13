5e0a5edffed040a85dbe9eaaef725faf7e3ffc1c
JWNg <JWNg@users.noreply.github.com>
2017-06-13 06:14:16 -0400

    Create automation(Part2).js

---
+++ kyu6/automation(Part2).js
@@ -0,0 +1,31 @@
+Preamble:
+
+You were cleaning your virtual pantry and accidentally found a folder with a label on it: "Automaton v1.9".
+Part 2:
+
+After you're done with cleaning you noticed Manual.txt right next to Automaton's folder.
+Manual for Automaton v1.9:
+
+- If automaton accepts "string" it will tell you the amount of vowels that string contains.
+Example:
+
+automaton("String") // 1
+- If automaton accepts "number" it will tell you if this is a prime number. Example:
+Example:
+
+automaton(3); // true
+automaton(3.5); // false
+automaton(4); // false
+- If automaton accepts "object" it will give you another object with swapped keys / values
+Example:
+
+automaton({"a": "b"}); // {"b": "a"}
+automaton([1, 0]); // {"0": "1", "1": "0"}
+automaton([]); // []
+You checked the results that Automaton gave you and saw that the data was inaccurate. So you decided to fix it.
+
+Other episodes from series:
+
+Automaton (Part 1)
+Automaton (Part 3)
+BUGS
