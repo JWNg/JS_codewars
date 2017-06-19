c0f6d14e82351665527ab12b2d7e3e1026bdab13
JWNg <JWNg@users.noreply.github.com>
2017-06-19 08:31:15 -0400

    Update highestScoringWord.js

--- kyu6/highestScoringWord.js
+++ kyu6/highestScoringWord.js
@@ -8,3 +8,14 @@ If two words score the same, return the word that appears earliest in the origin
 
 All letters will be lower case and all inputs will be valid.
 FUNDAMENTALSSTRINGSARRAYSNUMBERS
+
+
+function high(x){
+    return x.split(' ').reduce((accum, current)=>{
+        return score(current) > score(accum)? current:accum;  
+    })
+}
+
+function score(word){
+    return word.split('').reduce((accum,current)=>{return accum+(current.charCodeAt()-96)},0)
+}
