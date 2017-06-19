2b25e60d051a9484e4b7dc87c8b7dc8333d15371
JWNg <JWNg@users.noreply.github.com>
2017-06-19 07:54:14 -0400

    Create return1,2,3Randomly.js

---
+++ kyu6/return1,2,3Randomly.js
@@ -0,0 +1,4 @@
+You have function one_two that returns 1 or 2 with 50% chance.
+
+Your goal is to create function one_two_three that returns 1, 2 or 3 with equal probability using only one_two function.
+PUZZLES
