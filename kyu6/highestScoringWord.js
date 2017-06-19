c1fa82e10be29c86e7b84fe6e9b30e636680f6bb
JWNg <JWNg@users.noreply.github.com>
2017-06-19 08:11:31 -0400

    Create highestScoringWord.js

---
+++ kyu6/highestScoringWord.js
@@ -0,0 +1,10 @@
+Given a string of words (x), you need to find the highest scoring word.
+
+Each letter of a word scores points according to it's position in the alphabet. a=1, z=26 and everything inbetween.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lower case and all inputs will be valid.
+FUNDAMENTALSSTRINGSARRAYSNUMBERS
