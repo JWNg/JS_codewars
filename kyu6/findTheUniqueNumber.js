dc32dd77e773d389384e15b694ac52421903a4f4
JWNg <JWNg@users.noreply.github.com>
2017-06-20 10:39:53 -0400

    Update findTheUniqueNumber.js

--- kyu6/findTheUniqueNumber.js
+++ kyu6/findTheUniqueNumber.js
@@ -10,3 +10,9 @@ Find the unique number (this kata)
 Find the unique string
 Find The Unique
 FUNDAMENTALSALGORITHMSNUMBERSARRAYS
+
+
+function findUniq(arr) {
+  arr.sort((a,b)=>a-b);
+  return arr[0]==arr[1]?arr.pop():arr[0]
+}
