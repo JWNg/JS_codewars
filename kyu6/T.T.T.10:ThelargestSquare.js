9a90645dc85d03a0592326c940186d7e4b64e62d
JWNg <JWNg@users.noreply.github.com>
2017-06-22 10:03:07 -0400

    Create T.T.T.10:ThelargestSquare.js

---
+++ kyu6/T.T.T.10:ThelargestSquare.js
@@ -0,0 +1,16 @@
+They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. series #10:
+The largest square
+
+Task
+
+Give you a 2D array ```wall```:
+```
+[ ["X"," "," "," "," "," "], [" "," "," "," "," "," "], [" "," "," "," "," "," "], [" "," "," "," "," "," "], [" "," "," "," "," "," "], [" "," "," "," "," "," "] ]
+
+```
+
+```" "``` is the blank part, ```"X"``` is the hole in the wall, please find the largest square (no hole) on the wall, return its area.
+
+The above example should return ```25```, because the maximum square that can be found is ```5X5```
+
+Please see more example in testcases.
