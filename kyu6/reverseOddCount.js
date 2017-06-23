2dfce7c2bdd97216b8296387855b43bad94bbbaa
JWNg <JWNg@users.noreply.github.com>
2017-06-23 09:29:01 -0400

    Update reverseOddCount.js

--- kyu6/reverseOddCount.js
+++ kyu6/reverseOddCount.js
@@ -6,3 +6,15 @@ Reverse all elements in an array that occur an odd number of times.
 
 Since there are 2 a's and 2 c's, they will stay in place. There is 1 d and 1 b, so they will swap places.
 FUNDAMENTALS
+
+
+function reverseOddCount(array){
+    var reversed = array.filter((current)=>{return count(array, current)%2 === 1})
+    return array.map((current)=>{
+        return count(array, current)%2===1?reversed.pop():current;
+    })    
+}
+
+function count(array, obj){
+    return array.filter((current)=>{return obj === current}).length
+}
