2b8b13dd87529084ce23ad699d5608b852afe0b4
JWNg <JWNg@users.noreply.github.com>
2017-06-23 09:09:13 -0400

    Create reverseOddCount.js

---
+++ kyu6/reverseOddCount.js
@@ -0,0 +1,8 @@
+Reverse all elements in an array that occur an odd number of times.
+
+['a', 'a', 'b', 'c', 'c', 'd'] will output:
+
+['a', 'a', 'd', 'c', 'c', 'b']
+
+Since there are 2 a's and 2 c's, they will stay in place. There is 1 d and 1 b, so they will swap places.
+FUNDAMENTALS
