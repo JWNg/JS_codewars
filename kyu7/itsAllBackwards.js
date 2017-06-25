4368a8351ec3b3cab689d4484429efaf53f0755a
JWNg <JWNg@users.noreply.github.com>
2017-06-25 09:36:54 -0400

    Update itsAllBackwards.js

--- kyu7/itsAllBackwards.js
+++ kyu7/itsAllBackwards.js
@@ -4,3 +4,7 @@ You give me back this: 'examplE a iS thiS'
 
 So. I want what I give you back in reverse order, with only the last letter of each item capitalized. If the item is a single letter word and not capitilized when I give it to you, do not capitalize it.
 FUNDAMENTALSSTRINGS
+
+                  
+                  
+const flipper=stringArr=>stringArr.map(v => v.slice(0,-1).toLowerCase() + (v.length >1 ? v.substr(-1).toUpperCase() : v)).reverse().join(' ')
