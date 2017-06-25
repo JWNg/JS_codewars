919d8d72881d5bd188803a90091592df41eff779
JWNg <JWNg@users.noreply.github.com>
2017-06-25 09:22:50 -0400

    Create itsAllBackwards.js

---
+++ kyu7/itsAllBackwards.js
@@ -0,0 +1,6 @@
+I give you this: ['This', 'Is', 'a' 'Example');
+
+You give me back this: 'examplE a iS thiS'
+
+So. I want what I give you back in reverse order, with only the last letter of each item capitalized. If the item is a single letter word and not capitilized when I give it to you, do not capitalize it.
+FUNDAMENTALSSTRINGS
