06dec230bd467f106c70dcf0951e8a712afbfc23
JWNg <JWNg@users.noreply.github.com>
2017-06-26 09:15:18 -0400

    Create withoutTheLetterE.js

---
+++ kyu7/withoutTheLetterE.js
@@ -0,0 +1,11 @@
+Is it possible to write a book without the letter 'e' ?
+
+Task
+
+Given String str, return:
+
+How much "e" does it contains (case-insensitive) - return number as String.
+If given String doesn't contain any "e", return: There is no "e".
+If given String is empty, return empty String.
+If given String is null, return null
+FUNDAMENTALS
