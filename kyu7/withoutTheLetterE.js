17827fa6bfc1a2538225d144377219ba1d5fb5b7
JWNg <JWNg@users.noreply.github.com>
2017-06-26 09:19:54 -0400

    Update withoutTheLetterE.js

--- kyu7/withoutTheLetterE.js
+++ kyu7/withoutTheLetterE.js
@@ -9,3 +9,10 @@ If given String doesn't contain any "e", return: There is no "e".
 If given String is empty, return empty String.
 If given String is null, return null
 FUNDAMENTALS
+
+function findE(str){
+    if(str === null) {return null}
+    if(str.length === 0) {return ''}
+    let count = str.split('').filter((letter)=>{return letter === 'e' || letter === 'E'}).length
+    return count>0? count+'':'There is no "e".'
+}
