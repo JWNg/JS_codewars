4597a2160cbc62a4c151b0c6388956540210cd29
JWNg <JWNg@users.noreply.github.com>
2017-07-17 07:42:41 -0400

    Update D&DCharacterGenerator#1:AttributeModifiersAndSpells.js

--- kyu6/D&DCharacterGenerator#1:AttributeModifiersAndSpells.js
+++ kyu6/D&DCharacterGenerator#1:AttributeModifiersAndSpells.js
@@ -10,7 +10,7 @@ charAttribute(1) === {modifier: -5, maximumSpellLevel: -1, extraSpells: []}
 charAttribute(5) === {modifier: -3, maximumSpellLevel: -1, extraSpells: []}
 charAttribute(10) === {modifier: 0, maximumSpellLevel: 0, extraSpells: []}
 charAttribute(20) === {modifier: +5, maximumSpellLevel: 9, extraSpells: [2,1,1,1,1]}
-Note: I didn't explain things in detail and just pointed out to the table on purpose, as my goal is also to train the pattern recognition skills of whoever is going to take this challenges, so do not complain about a summary description. Thanks :)
+Note: I didnt explain things in detail and just pointed out to the table on purpose, as my goal is also to train the pattern recognition skills of whoever is going to take this challenges, so do not complain about a summary description. Thanks :)
 
 In the same series:
 
@@ -18,3 +18,13 @@ D&D Character generator #1: attribute modifiers and spells
 D&D Character generator #2: psion power points
 D&D Character generator #3: carrying capacity
 FUNDAMENTALS
+
+function charAttribute(n){
+  const m = Math.floor(n/2) - 5,
+      ix = (m/4) | 0,
+      modifier = n ? m : 0,
+      extraSpells = Array.from({length:m % 4},e => ix+1).concat(Array.from({length:Math.min(4*ix,9)},(e,i) => ix-((i/4)|0))).slice(0,9),
+      maximumSpellLevel = Math.max(Math.min(extraSpells.length + m + (n < 9 ? 0 : n%2),9),-1);
+  
+  return {modifier,maximumSpellLevel,extraSpells};
+}
