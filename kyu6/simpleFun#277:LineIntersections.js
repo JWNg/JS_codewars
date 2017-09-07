22d4216f4dc8daff82b5c5e0845eb61dbfe33644
JWNg <JWNg@users.noreply.github.com>
2017-09-07 17:57:53 -0400

    Create simpleFun#277:LineIntersections.js

---
+++ kyu6/simpleFun#277:LineIntersections.js
@@ -0,0 +1,30 @@
+Task
+
+Given some line segments lying on x-axis with starting points start and end points end, find the number of pairs of lines which overlaps.
+
+Input/Output
+
+[input] integer array start
+
+The starting points of the segments. For each i start[i] ≥ 0.
+
+[input] integer array end
+
+The ending points of the segments. end[i] is greater than start[i].
+
+[output] an integer
+
+The number of pairs of overlapping line segments.
+
+Example
+
+For start = [8, 4, 6, 1] and end = [10, 9, 7, 2],
+
+the output should be 2.
+
+The two intersecting lines are:
+
+(4, 9) intersects with (6, 7);
+(4, 9) intersects with (8,10).
+For start = [1, 2] and end = [3, 4], the output should be 1.
+ALGORITHMS
